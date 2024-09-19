e283788a99e72387f9e533433bfefd7687ce3f45
Diego Costa <diegocosta.contato2021@gmail.com>
2024-09-19 08:53:36 -0300

    Inserindo novo arquivo contendo Inserts de planos

---
+++ Database/Inserts.sql
@@ -0,0 +1,71 @@
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    1,
+    'Dental Júnior',
+    '• Rol ANS. • Limpeza. • Extração de siso. • Tratamento e retratamento de canal.',
+    29.90
+);
+
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    2,
+    'Bem Estar',
+    '• Rol ANS. • Extração de siso. • Tratamento e retratamento de canal. • Prótese Simples.',
+    45.60
+);
+
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    3,
+    'Bem Estar White',
+    '• Rol ANS. • Extração de siso. • Tratamento e retratamento de canal. • Placa de bruxismo. • Clareamento estético com Gel.',
+    89.90
+);
+
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    4,
+    'Bem Estar Pro',
+    '• Rol ANS. • Extração de siso. • Prótese removível total (dentadura). • Prótese parcial removível.',
+    99.90
+);
+
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    5,
+    'Bem Estar Orto',
+    '• Rol ANS. • Aparelho Fixo e Móvel. • Manutenção de aparelhos. • Documentação ortodôntica completa.',
+    139.90
+);
+
+INSERT INTO CH_CONVENIO (
+    CONVENIO_ID,
+    NOME_CONVENIO,
+    DESCRICAO,
+    PRECO
+) VALUES (
+    6,
+    'Bem Estar Orto White',
+    '• Rol ANS. • Aparelho Fixo e Móvel. • Manutenção de aparelhos. • Documentação ortodôntica completa. • Clareamento estético com Gel.',
+    159.90
+);
